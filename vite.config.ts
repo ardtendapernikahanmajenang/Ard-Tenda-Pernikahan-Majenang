a05990c80ee8a123d528cdfa3002014f26bb1756
ardtendapernikahan <ardtendapernikahan@gmail.com>
2024-07-26 03:28:34 +0700

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import type { UserConfig } from 'vite';
-
-// https://vitejs.dev/config/
-export default defineConfig((): UserConfig => ({
-  plugins: [react()],
-}));
-
+import vercel from 'vite-plugin-vercel';
+ 
+export default defineConfig({
+  server: {
+    port: process.env.PORT as unknown as number,
+  },
+  plugins: [vercel()],
+});
