cae5de2cfc60490fa18bd20a7a9411cf5c077992
ardtendapernikahan <ardtendapernikahan@gmail.com>
2024-07-26 05:56:55 +0700

    update again vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 // src/App.tsx
+// src/App.tsx
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
 import Theme from "./chakra";
+
 const App = () => {
   return (
     <ChakraProvider theme={Theme}>
-      <Home />
+      <Router basename={import.meta.env.BASE_URL}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Tambahkan rute lain sesuai kebutuhan */}
+        </Routes>
+      </Router>
     </ChakraProvider>
   );
 };
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string;
+  // Tambahkan variabel lingkungan lainnya jika ada
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 
